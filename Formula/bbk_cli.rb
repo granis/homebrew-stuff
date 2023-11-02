a2a02548e2a2b0260036c683bd4474b472c4bb34
granis <granis@gmail.com>
2023-11-02 21:25:23 +0100

    chore: update to bbk-cli 1.2

--- Formula/bbk_cli.rb
+++ Formula/bbk_cli.rb
@@ -1,12 +1,17 @@
 class BbkCli < Formula
   desc "Unofficial formula for Bredbandskollen CLI"
   homepage "https://github.com/dotse/bbk"
-  url "https://github.com/dotse/bbk/archive/3180567c5830c82c991a8059692d8fc3725df7cd.tar.gz"
-  version "1.1"
-  sha256 "0158403c6c822e6843aa25543ac977934e34398ddfbc6509dcc12747937d97ee"
+  url "https://github.com/dotse/bbk/archive/refs/tags/BBK_CLI_1.2.tar.gz"
+  version "1.2"
+  sha256 "dff38d1226844642891afcd869bfce2b9d0414c9507bacf156fb5849ab2ae8c5"
   license "GPL-2.0-or-later"
 
   depends_on "gnutls"
+
+  stable do
+    # update version string to match release tag
+    patch :DATA
+  end
 
   def install
     chdir "src/cli" do
@@ -19,3 +24,15 @@ class BbkCli < Formula
     assert_match "Bredbandskollen ", shell_output("#{bin}/bbk_cli --version", 1)
   end
 end
+
+__END__
+diff --git a/src/measurement/defs.h b/src/measurement/defs.h
+--- a/src/measurement/defs.h
++++ b/src/measurement/defs.h
+@@ -4,5 +4,5 @@
+
+ namespace measurement {
+     extern const std::string appName;
+-    const std::string appVersion = "1.1";
++    const std::string appVersion = "1.2";
+     // buildVersion 15 released to App Store in 2016.
